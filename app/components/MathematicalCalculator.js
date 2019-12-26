70c5b0dd274a729a42d8e872c561e03aa9d10d9c
John Shannon <jiang.h@ikcrm.com>
2019-12-26 18:43:49 +0800

    class

--- app/components/MathematicalCalculator.js
+++ app/components/MathematicalCalculator.js
@@ -1,29 +1,101 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import ReactTooltip from 'react-tooltip';
 import MainContainer from './MainContainer';
+import styles from './MathematicalCalculator.css';
 
-export default function() {
-  const [result, setResult] = useState(0);
-  function execute() {
-    setResult(2);
+type Props = {};
+
+export default class MathematicalCalculator extends React.Component<Props> {
+  constructor() {
+    super();
+    this.state = { result: 0, userstdin: '' };
   }
-  return (
-    <MainContainer>
-      <textarea id="stdin" name="stdin" />
-      <div id="cmd-buttons">
-        <button type="button">清空</button>
-        <button type="button" onClick={execute}>
-          运算
-        </button>
-      </div>
-      <div>{result && <span>result</span>}</div>
-      <div id="functions">
-        <button type="button" name="Math.pow" title="指数函数: Math.pow(x,y)">
-          x<sup>y</sup>
-        </button>
-        <button type="button" name="Math.sqrt" title="求X平方根: Math.sqrt(x)">
-          √x
-        </button>
-      </div>
-    </MainContainer>
-  );
+
+  execute() {
+    const { userstdin } = this.state;
+    // eslint-disable-next-line no-eval
+    const result = eval(userstdin);
+    this.setState({ result });
+  }
+
+  clear() {
+    this.setState({ userstdin: '', result: 0 });
+  }
+
+  appendFunc(methodName) {
+    const method = `${methodName}`;
+    const { userstdin } = this.state;
+    this.setState({ userstdin: userstdin + method });
+  }
+
+  setUserStdin = e => {
+    this.setState({ userstdin: e.target.value });
+  };
+
+  stdinKeyHandler = e => {
+    if (e.key === 'Enter') {
+      this.execute();
+    }
+  };
+
+  render() {
+    const { result, userstdin } = this.state;
+    return (
+      <MainContainer>
+        <textarea
+          id="stdin"
+          name="stdin"
+          value={userstdin}
+          onChange={this.setUserStdin}
+          className={styles.stdin}
+          onKeyPress={this.stdinKeyHandler}
+        />
+        <div className={styles.results}>{result && <span>{result}</span>}</div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              this.clear();
+            }}
+          >
+            清空
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              this.execute();
+            }}
+          >
+            运算
+          </button>
+        </div>
+        <div>
+          <button
+            data-tip="指数函数: x<sup>y</sup>。x的y次方转写为函数: Math.pow(x,y)"
+            type="button"
+            onClick={() => {
+              this.appendFunc('Math.pow(x,y)');
+            }}
+          >
+            x<sup>y</sup>
+          </button>
+          <button
+            data-tip="求X平方根: √x。x平方根方转写为函数: Math.sqrt(x)"
+            type="button"
+            onClick={() => {
+              this.appendFunc('Math.sqrt(x)');
+            }}
+          >
+            √x
+          </button>
+          <ReactTooltip
+            html={!!true}
+            place="right"
+            type="dark"
+            effect="float"
+          />
+        </div>
+      </MainContainer>
+    );
+  }
 }
